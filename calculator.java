c2504534bf9b3dd03f8d7f5a5a540a9c33a274bf
kasyan <kasyan.sardar@gmail.com>
2026-03-28 22:42:03 +0300

    Add files via upload

---
+++ calculator.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package lab1_sem4;
+
+/**
+ *
+ * @author Ari Tech
+ */
+public class calculator {
+    
+    
+    
+}
